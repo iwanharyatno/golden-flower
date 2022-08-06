ddb15922bd06762751c181680882f5ad7a12da30
iwanharyatno <iwnharry61@gmail.com>
2022-08-06 20:35:21 +0700

    Make the app functional

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
-const calyxes = document.querySelectorAll('.calyx');
+const PHI = (1 + Math.sqrt(5)) / 2;
+const PHI_ANGLE = 360 / (PHI + 1);
 
-let rotation = 137.5;
+let rotationDegree = PHI_ANGLE;
 
-for (const calyx of calyxes) {
-    calyx.style.transform = 'rotate(' + rotation + 'deg)';
-    rotation += 137.5;
+const increaseButton = document.getElementById('increase-calyxes');
+const decreaseButton = document.getElementById('decrease-calyxes');
+const calyxesInput = document.getElementById('calyxes');
+const flower = document.getElementById('flower');
+
+calyxesInput.addEventListener('change', renderCalyxes);
+
+increaseButton.addEventListener('click', function() {
+    calyxesInput.value = parseInt(calyxesInput.value) + 1;
+    renderCalyxes();
+});
+
+decreaseButton.addEventListener('click', function() {
+    calyxesInput.value = parseInt(calyxesInput.value) + 1;
+    renderCalyxes();
+});
+
+function renderCalyxes() {
+    let calyxes = parseInt(calyxesInput.value);
+    rotationDegree = PHI_ANGLE;
+    flower.innerHTML = '';
+
+    for (let i = 0; i < calyxes; i++) {
+        const calyx = document.createElement('div');
+        calyx.classList.add('calyx');
+        calyx.style.transform = 'rotate(' + rotationDegree + 'deg)';
+        flower.appendChild(calyx);
+        rotationDegree += PHI_ANGLE;
+    }
 }
+
+renderCalyxes();
